e581f0d4a36abc2e001dea8951d598b7295c675d
bellapacx <bellapacx@gmail.com>
2025-07-06 02:21:00 +0300

    23gdzfsfjhssdfgskh

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       
-      <body className="min-h-screen bg-white text-gray-900 flex flex-col">
+      <body className="min-h-screen bg-white text-gray-800 flex flex-col">
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
